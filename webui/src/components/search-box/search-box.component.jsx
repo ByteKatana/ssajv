3e9e2d9ccdfb631c38e3da92632e2212abe7c173
ByteKatana <19264901+ByteKatana@users.noreply.github.com>
2020-09-29 20:51:31 +0300

    update

--- webui/src/components/search-box/search-box.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
-
- export const SearchBox = ({placeholder, handleChange }) => (
-    <Form.Group>
-              <Form.Control size="lg" type="search" placeholder={placeholder} onChange={handleChange} />
-    </Form.Group>
-)
